00e9ada0cc3eab7accb99f7df9ad9a8e37aefc9c
robikbobik <robertjaros235@gmail.com>
2022-11-18 23:18:17 +0100

    create feed

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("feed/", views.index, name="index")
+    path("feed/", views.index, name="index"),
+    path("new_feed/", views.new_feed, name="new_feed"),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def index(request):
-    return HttpResponse("Index file")
+    feeds = models.Feed.objects.all().values()
+    print(feeds)
+    return render(request, "feed/index.html", {"feeds": feeds})
+
+def new_feed(request):
+    feeds = models.Feed.objects.all().values()
+    new_f = models.Feed(name=request.POST["feed_name"])
+    new_f.save()
+    return render(request, "feed/index.html", {"feeds": feeds})
